165c003e28e8435c4b44152b1cd12886d62bde05
Misom <misom@symlinks.dev>
2025-07-23 17:59:05 +0200

    add support for Fossil Gen 6
    Add support for Fossil Gen 6 (fossil-hoki), a smartwatch with sdm429w chipset.
    
    Signed-off-by: Misom <misom@symlinks.dev>

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ Supported devices:
 - Redmi 7 (xiaomi-onclite).
 - Redmi 7A (xiaomi-pine)
 - Lenovo Tab M10 HD (lenovo-tbx505x)
+- Fossil Gen 6 (fossil-hoki)
 
 Used for lk2nd and mainline kernel.
 
--- build.sh
+++ build.sh
@@ -7,3 +7,5 @@ dtc -O dtb -o sdm439-xiaomi-pine.dtbo -b 0 -@ sdm439-xiaomi-pine.dts
 mkdtboimg cfg_create dtbo-xiaomi-pine.img dtboimg-xiaomi-pine.cfg
 dtc -O dtb -o sdm429-lenovo-tbx505x.dtbo -b 0 -@ sdm429-lenovo-tbx505x.dts
 mkdtboimg cfg_create dtbo-lenovo-tbx505x.img dtboimg-lenovo-tbx505x.cfg
+dtc -O dtb -o sdm429-fossil-hoki.dtbo -b 0 -@ sdm429-fossil-hoki.dts
+mkdtboimg cfg_create dtbo-fossil-hoki.img dtboimg-fossil-hoki.cfg
