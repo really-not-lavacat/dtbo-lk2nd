3eafb177cb700a0a26f48549c978a32d82d3539d
Biswapriyo Nath <nathbappai@gmail.com>
2025-08-19 05:30:59 +0000

    add support for Nokia 4.2
    Signed-off-by: Biswapriyo Nath <nathbappai@gmail.com>

--- README.md
+++ README.md
@@ -6,6 +6,7 @@ Supported devices:
 - Redmi 7A (xiaomi-pine)
 - Lenovo Tab M10 HD (lenovo-tbx505x)
 - Fossil Gen 6 (fossil-hoki)
+- HMD Global Nokia 4.2 (nokia-panther)
 
 Used for lk2nd and mainline kernel.
 
--- build.sh
+++ build.sh
@@ -9,3 +9,5 @@ dtc -O dtb -o sdm429-lenovo-tbx505x.dtbo -b 0 -@ sdm429-lenovo-tbx505x.dts
 mkdtboimg cfg_create dtbo-lenovo-tbx505x.img dtboimg-lenovo-tbx505x.cfg
 dtc -O dtb -o sdm429-fossil-hoki.dtbo -b 0 -@ sdm429-fossil-hoki.dts
 mkdtboimg cfg_create dtbo-fossil-hoki.img dtboimg-fossil-hoki.cfg
+dtc -O dtb -o sdm439-nokia-panther.dtbo -b 0 -@ sdm439-nokia-panther.dts
+mkdtboimg cfg_create dtbo-nokia-panther.img dtboimg-nokia-panther.cfg
